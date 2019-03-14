b56fa9e209d4885260aa4a7dfd4c861451773b20
小黑也叫小白 <834571627@qq.com>
2019-03-14 17:40:18 +0800

    no message

--- src/page/study/Index.tsx
+++ src/page/study/Index.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { HashRouter as Router, Route, Switch, Redirect, Link} from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import { CSSTransition } from 'react-transition-group';
 import 'animate.css';
 import './Index.scss';
